33d77c4a237a8b1c526c0f9adc753c6a863d3ce8
Vladimir Sharapov <vladimir.sharapov@shoppertrak.com>
2021-03-14 13:52:03 -0500

    added "test_add_contact.py" and excluded class to store contacts property separately

---
+++ add_new_form.py
@@ -0,0 +1,8 @@
+
+
+class AddNewForm:
+    def __init__(self, first_name, last_name, address, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.email = email
---
+++ test_add_contact.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import unittest
+from add_new_form import AddNewForm
+
+class TestAddContact(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook")
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_id("LoginForm").submit()
+
+    def open_add_new_page(self, wd):
+        wd.find_element_by_link_text("add new").click()
+
+    def create_new_contact(self, wd, add_new_form):
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(add_new_form.first_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(add_new_form.last_name)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(add_new_form.address)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(add_new_form.email)
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def return_to_home_page(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_add_new_page(wd)
+        self.create_new_contact(wd, AddNewForm(first_name="Vladimir", last_name="Sharapov", address="spb", email="test@gmail.com"))
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == "__main__":
+    unittest.main()
