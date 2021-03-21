817f9d1ba1def1234abe29a5257facb40fdda299
Volody47 <volody.sharapov@yandex.ru>
2021-03-21 15:15:21 -0500

    initialize webdriver(wd) from functional methods

--- test_add_contact.py
+++ test_add_contact.py
@@ -8,20 +8,24 @@ class TestAddContact(unittest.TestCase):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook")
 
-    def login(self, wd, username, password):
+    def login(self, username, password):
+        wd = self.wd
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_id("LoginForm").submit()
 
-    def open_add_new_page(self, wd):
+    def open_add_new_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("add new").click()
 
-    def create_new_contact(self, wd, add_new_form):
+    def create_new_contact(self, add_new_form):
+        wd = self.wd
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(add_new_form.first_name)
@@ -36,21 +40,22 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("email").send_keys(add_new_form.email)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def return_to_home_page(self, wd):
+    def return_to_home_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("home page").click()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
 
     def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_add_new_page(wd)
-        self.create_new_contact(wd, AddNewForm(first_name="Vladimir", last_name="Sharapov", address="spb", email="test@gmail.com"))
-        self.return_to_home_page(wd)
-        self.logout(wd)
+        self.open_home_page()
+        self.login(username="admin", password="secret")
+        self.open_add_new_page()
+        self.create_new_contact(AddNewForm(first_name="Vladimir", last_name="Sharapov", address="spb", email="test@gmail.com"))
+        self.return_to_home_page()
+        self.logout()
 
 
     def tearDown(self):
