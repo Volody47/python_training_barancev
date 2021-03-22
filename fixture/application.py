d47e5d3d6bb96a4f5d1d507d9d43a6ee67094ab7
Volody47 <volody.sharapov@yandex.ru>
2021-03-21 22:48:03 -0500

    created session.py with class SessionHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,22 +1,18 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
+
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
 
-    def login(self, username, password):
-        wd = self.wd
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_groups_page(self):
         wd = self.wd
@@ -67,9 +63,6 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("home page").click()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -10,10 +10,9 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.open_add_new_page()
     app.create_new_contact(AddNewForm(first_name="Vladimir", last_name="Sharapov", address="spb", email="test@gmail.com"))
     app.return_to_home_page()
-    app.logout()
+    app.session.logout()
 
