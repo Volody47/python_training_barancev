19d7ea20f01f7aa7d66fa6a2773fb7f0a890491c
Volody47 <volody.sharapov@yandex.ru>
2021-03-21 22:57:34 -0500

    created group.py with class GroupHelper

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -11,16 +11,16 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.open_groups_page()
-    app.create_group(Group(name="gfsdgsdhs", header="sdfsvds", footer="sdssvs"))
-    app.return_to_groups_page()
+    app.group.open_groups_page()
+    app.group.create(Group(name="gfsdgsdhs", header="sdfsvds", footer="sdssvs"))
+    app.group.return_to_groups_page()
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.open_groups_page()
-    app.create_group(Group(name="", header="", footer=""))
-    app.return_to_groups_page()
+    app.group.open_groups_page()
+    app.group.create(Group(name="", header="", footer=""))
+    app.group.return_to_groups_page()
     app.session.logout()
 
 
