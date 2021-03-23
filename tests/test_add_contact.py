9dad68a6cdaad4023ca758b50c5b1f72f414b811
Volody47 <volody.sharapov@yandex.ru>
2021-03-22 22:24:23 -0500

    created conftest.py and post fixture inside

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.add_new_form import AddNewForm
-from fixture.application import Application
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
