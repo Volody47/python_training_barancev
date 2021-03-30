6024f1678b641ded29bbad6f4a1c740e57dedd3a
Volody47 <volody.sharapov@yandex.ru>
2021-03-29 21:32:35 -0500

    excluded method def fill_out_group_form

--- fixture/group.py
+++ fixture/group.py
@@ -10,12 +10,8 @@ class GroupHelper:
         wd.find_element_by_link_text("groups").click()
 
 
-    def create(self, group):
+    def fill_out_group_form(self, group):
         wd = self.app.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -25,9 +21,18 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        self.fill_out_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+
 
 
     def select_first_group(self):
@@ -50,12 +55,7 @@ class GroupHelper:
         self.select_first_group()
         # edit group
         wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_out_group_form(group)
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
