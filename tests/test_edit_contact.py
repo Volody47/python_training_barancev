c315853aac5deff949007cfc101180724775c854
Volody47 <volody.sharapov@yandex.ru>
2021-03-30 22:51:13 -0500

    updated conftest.py, excluded login, logout from group tests and also created new method def is_valid

--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -1,11 +1,7 @@
 from model.add_new_form import AddNewForm
 
 def test_edit_contact_first_name(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(AddNewForm(first_name="Vlad"))
-    app.session.logout()
 
 def test_edit_contact_address(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(AddNewForm(address="Chicago"))
-    app.session.logout()
