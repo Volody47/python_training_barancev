58ed46f90d19d95eff39812ba40e35e76bbff26c
Volody47 <volody.sharapov@yandex.ru>
2021-03-30 22:59:29 -0500

    excluded login, logout from contact tests

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -4,7 +4,5 @@ from model.add_new_form import AddNewForm
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create_new_contact(AddNewForm(first_name="Vladimir", last_name="Sharapov", address="spb", email="test@gmail.com"))
-    app.session.logout()
 
