6fe08138b3316e3f53a3d235e05ccf0584b7a7bb
Volody47 <volody.sharapov@yandex.ru>
2021-03-31 22:17:09 -0500

    deleted logout() method from tests, because it's was for testing purpose

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -5,7 +5,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="gfsdgsdhs", header="sdfsvds", footer="sdssvs"))
-    app.session.logout()
+
 
 
 def test_add_empty_group(app):
