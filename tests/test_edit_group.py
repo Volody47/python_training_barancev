9596dea3b77ed090d6759f7b6011b9afd031a24b
Volody47 <volody.sharapov@yandex.ru>
2021-03-31 23:11:30 -0500

    added logic for test_edit_group if we don't have any group

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -1,7 +1,12 @@
 from model.group import Group
 
 def test_edit_group_name(app):
+    if app.group.count_group() == 0:
+        app.group.create(Group(name="gfsdgsdhs"))
     app.group.edit_first_group(Group(name="New group"))
 
+
 def test_edit_group_header(app):
+    if app.group.count_group() == 0:
+        app.group.create(Group(header="sdfsvds"))
     app.group.edit_first_group(Group(header="New header"))
