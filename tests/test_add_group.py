a2db0eb7242fda5ea63dd17eb2f39e56d79dda7f
Volody47 <volody.sharapov@yandex.ru>
2021-03-31 22:15:41 -0500

    added logic for login

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -5,9 +5,11 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="gfsdgsdhs", header="sdfsvds", footer="sdssvs"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
+
 
 
