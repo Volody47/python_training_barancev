5e4c84cc58a69d5e46434e4b5ed3748fd6a8bdfb
Volody47 <volody.sharapov@yandex.ru>
2021-03-31 22:51:39 -0500

    added logic for test_delete_contact.py if we don't have any contact

--- tests/test_delete_contact.py
+++ tests/test_delete_contact.py
@@ -1,5 +1,7 @@
-
+from model.add_new_form import AddNewForm
 
 
 def test_delete_contact(app):
+    if app.contact.count_contact() == 0:
+        app.contact.create_new_contact(AddNewForm(first_name="Vladimir"))
     app.contact.delete_first_contact()
