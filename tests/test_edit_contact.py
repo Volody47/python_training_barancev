3b8525bf3b8852a1b42345f8252b1b935c1ee79b
Volody47 <volody.sharapov@yandex.ru>
2021-03-31 23:13:43 -0500

    added logic for test_edit_contact if we don't have any contact

--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -1,7 +1,12 @@
 from model.add_new_form import AddNewForm
 
 def test_edit_contact_first_name(app):
+    if app.contact.count_contact() == 0:
+        app.contact.create_new_contact(AddNewForm(first_name="Vladimir"))
     app.contact.edit_first_contact(AddNewForm(first_name="Vlad"))
 
+
 def test_edit_contact_address(app):
+    if app.contact.count_contact() == 0:
+        app.contact.create_new_contact(AddNewForm(address="spb"))
     app.contact.edit_first_contact(AddNewForm(address="Chicago"))
