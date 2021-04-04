cab8a108fbed358d0adab9f6a0f32eb35e36164f
Volody47 <volody.sharapov@yandex.ru>
2021-04-04 17:00:54 -0500

    add verify(assert) list of groups added/deleted/edited

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -1,12 +1,18 @@
 from model.group import Group
 
 def test_edit_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count_group() == 0:
         app.group.create(Group(name="gfsdgsdhs"))
     app.group.edit_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count_group() == 0:
         app.group.create(Group(header="sdfsvds"))
     app.group.edit_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
