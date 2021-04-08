82fed095667fb310102d1e395ee3c5005df54634
Volody47 <volody.sharapov@yandex.ru>
2021-04-07 23:03:38 -0500

    updated verification for test_delete_contact.py

--- model/add_new_form.py
+++ model/add_new_form.py
@@ -7,3 +7,9 @@ class AddNewForm:
         self.address = address
         self.email = email
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s:%s:%s" % (self.id, self.first_name, self.last_name, self.address)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.first_name == other.first_name and self.last_name == other.last_name and self.address == other.address
