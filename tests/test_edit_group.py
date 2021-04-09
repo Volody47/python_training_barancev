0ebc4368b74ea0d58f2ea8466973ebf19ba2baf6
Volody47 <volody.sharapov@yandex.ru>
2021-04-08 23:27:56 -0500

    added verification for test_edit_group.py

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -1,18 +1,26 @@
 from model.group import Group
 
 def test_edit_group_name(app):
-    old_groups = app.group.get_group_list()
     if app.group.count_group() == 0:
         app.group.create(Group(name="gfsdgsdhs"))
-    app.group.edit_first_group(Group(name="New group"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="New group")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_edit_group_header(app):
-    old_groups = app.group.get_group_list()
     if app.group.count_group() == 0:
         app.group.create(Group(header="sdfsvds"))
-    app.group.edit_first_group(Group(header="New header"))
+    old_groups = app.group.get_group_list()
+    group = Group(header="New header")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
