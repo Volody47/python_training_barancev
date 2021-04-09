cab547546bd46f85875f7581c871342ddf9f2c9f
Volody47 <volody.sharapov@yandex.ru>
2021-04-08 22:47:07 -0500

    updated verification for test_add_group.py

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from sys import maxsize
 
 
 def test_add_group(app):
@@ -10,12 +9,7 @@ def test_add_group(app):
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
-    def id_or_max(gr):
-        if gr.id:
-            return int(gr.id)
-        else:
-            return maxsize
-    assert sorted(old_groups, key=id_or_max) == sorted(new_groups, key=id_or_max)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
@@ -26,12 +20,7 @@ def test_add_empty_group(app):
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
-    def id_or_max(gr):
-        if gr.id:
-            return int(gr.id)
-        else:
-            return maxsize
-    assert sorted(old_groups, key=id_or_max) == sorted(new_groups, key=id_or_max)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
