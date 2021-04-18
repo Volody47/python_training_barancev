f20c1c9fe80fd45acbe5d1bb92d05fba156f1168
Volody47 <volody.sharapov@yandex.ru>
2021-04-18 15:32:05 -0500

    updated test_add_contact.py paramenrize to use random data from data_for_contact.py

--- resourses/data_for_contact.py
+++ resourses/data_for_contact.py
@@ -1,8 +1,13 @@
 from model.add_new_form import AddNewForm
+import random
+import string
 
 
-test_contact_data = [
-    AddNewForm(first_name="Vladimir", last_name="Sharapov", address="spb", email="test@gmail.com"),
-    AddNewForm(first_name="", last_name="", address="", email=""),
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
+test_contact_data = [AddNewForm(first_name="", last_name="", address="", email="")] + [
+    AddNewForm(first_name=random_string("first_name:", 10), last_name=random_string("last_name:", 10), address=random_string("address:", 10), email=random_string("email:", 10))
+    for i in range(5)
 ]
