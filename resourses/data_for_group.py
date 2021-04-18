86e8aa85381dcfa590b76b89b55882535a650715
Volody47 <volody.sharapov@yandex.ru>
2021-04-18 15:22:41 -0500

    updated test_add_group.py paramenrize to use random data from data_for_group.py

--- resourses/data_for_group.py
+++ resourses/data_for_group.py
@@ -1,7 +1,14 @@
 from model.group import Group
+import random
+import string
 
 
-test_group_data = [
-    Group(name="gfsdgsdhs", header="sdfsvds", footer="sdssvs"),
-    Group(name="", header="", footer="")
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+test_group_data = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name:", 10), header=random_string("header:", 20), footer=random_string("footer:", 20))
+    for i in range(5)
 ]
