dc46ffbbbb7cff48bdfb9fb3c210f4d56c588759
Volody47 <volody.sharapov@yandex.ru>
2021-04-25 08:40:19 -0500

    created generator_contact.py and implemented command line option for generate contact

---
+++ generator/generator_contact.py
@@ -0,0 +1,40 @@
+from model.add_new_form import AddNewForm
+import random
+import string
+import os.path
+import jsonpickle
+import getopt
+import sys
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contact", "file"])
+except getopt.GetoptError as err:
+    # print help information and exit:
+    print(err)  # will print something like "option -a not recognized"
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "resourses/contact.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+test_contact_data = [AddNewForm(first_name="", last_name="", address="", email="")] + [
+    AddNewForm(first_name=random_string("first_name:", 10), last_name=random_string("last_name:", 10), address=random_string("address:", 10), email=random_string("email:", 10))
+    for i in range(n)
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(test_contact_data))
