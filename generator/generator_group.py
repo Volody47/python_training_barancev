cdb84161c0a643faeebe76445a4a0d19a78deb81
Volody47 <volody.sharapov@yandex.ru>
2021-04-24 23:05:24 -0500

    updated generator_group.py in order to generate group.json file

--- generator/generator_group.py
+++ generator/generator_group.py
@@ -1,8 +1,8 @@
 from model.group import Group
 import random
 import string
-
-
+import os.path
+import json
 
 
 def random_string(prefix, maxlen):
@@ -14,3 +14,7 @@ test_group_data = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name:", 10), header=random_string("header:", 20), footer=random_string("footer:", 20))
     for i in range(5)
 ]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../resourses/group.json")
+with open(file, "w") as f:
+    f.write(json.dumps(test_group_data, default=lambda x: x.__dict__, indent=2))
