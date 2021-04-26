a51a637029003b0ff9e411972051a47fad82523d
Volody47 <volody.sharapov@yandex.ru>
2021-04-25 23:00:59 -0500

    implemented functionality to parametrize test in order to use data from .json file for contact

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -4,8 +4,9 @@ from resourses.data_for_contact import test_contact_data
 import pytest
 
 
-@pytest.mark.parametrize("contact", test_contact_data, ids=[repr(x) for x in test_contact_data])
-def test_add_contact(app, contact):
+#@pytest.mark.parametrize("contact", test_contact_data, ids=[repr(x) for x in test_contact_data])
+def test_add_contact(app, json_contact):
+    contact = json_contact
     old_contacts = app.contact.get_contact_list_version2()
     app.contact.create_new_contact(contact)
     new_contacts = app.contact.get_contact_list_version2()
