7aa76835a05a1edc846608d5f2aeafee71d91f38
Volody47 <volody.sharapov@yandex.ru>
2021-05-05 22:21:36 -0500

    reduced time of execution for test_add_group.py, instead get_group_list from web page got it from db

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -5,11 +5,11 @@ import pytest
 
 
 #@pytest.mark.parametrize("group", test_group_data, ids=[repr(x) for x in test_group_data])
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.create(group)
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     assert len(old_groups) + 1 == app.group.count_group()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
